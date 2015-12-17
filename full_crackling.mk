63ba940ffabbd78098b356a3cfc46fa40edcdc4d
Adam Farden <adam@farden.cz>
2015-12-17 21:27:35 +0100

    crackling: initial Firefox OS commit

--- full_crackling.mk
+++ full_crackling.mk
@@ -21,10 +21,33 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from crackling device
 $(call inherit-product, device/wileyfox/crackling/device.mk)
 
+# for Gecko to use the correct resolution assets
+# Valid options are: 1.5 | 2 | 2.25
+GAIA_DEV_PIXELS_PER_PX := 2
+
+# for Gecko to use the correct boot animation
+# Valid options are: hvga | fwvga | qHD | 720p | 1080p
+BOOTANIMATION_ASSET_SIZE := 720p
+
+# for Gecko to support usb mass storage
+# You may need to add mass_storage to init.oem.usb.rc
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mass_storage
+
+# for Gecko to support virtual home button
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.moz.has_home_button=0
+
+# for Gecko to support Dual SIM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.multisim.config=dsds \
+    ro.moz.ril.numclients=2 \
+    ro.moz.ril.subscription_control=true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := crackling
 PRODUCT_NAME := full_crackling
-PRODUCT_BRAND := wileyfox
-PRODUCT_MODEL := CRACKLING
-PRODUCT_MANUFACTURER := wileyfox
+PRODUCT_BRAND := Wileyfox
+PRODUCT_MODEL := Wileyfox Swift
+PRODUCT_MANUFACTURER := Wileyfox
 
